1374c7cefda6edd056224ce68f1fd8f89c89bfbe
Sujata kumari <sujata.kumari@innovatechs.com>
2022-12-15 14:48:28 +0530

    new changes

--- substraction.java
+++ substraction.java
@@ -4,7 +4,7 @@ int num1=35,num2=56,sub;
 //declare and initialize variables num1,num2,sub;
 sub=num2-num1;//find subtract of two numbers
 System.out.println("Subtract of two numbers: "+sub);
-//display the subtraction of two numners
+//display the subtraction of two numner
 
 }
 }
